ae7a9663e85d4621d4e0c6cff692ae7aeb0b8f4b
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-01-14 20:48:08 -0600

    removed self.playlists array

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -104,14 +104,6 @@ angular.module('starter.controllers', [])
   // console.log("LOCAL STORAGE IS ", self.noteHistory);
 
 
-  self.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ]
 
   self.notes;
 
